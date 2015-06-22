aaa5be253b8d64ec031ba4a2252475321e275647
Tim Lundqvist <timluq@gmail.com>
2015-06-23 00:44:11 +0200

    filehandling and type 2

--- src/Web/Authenticate/SQRL/SecureStorage.hs
+++ src/Web/Authenticate/SQRL/SecureStorage.hs
@@ -4,58 +4,97 @@ module Web.Authenticate.SQRL.SecureStorage where
 import Web.Authenticate.SQRL
 import Web.Authenticate.SQRL.Client
 
+-- | Type 1 - User access password authenticated & encrypted data
+--
+-- The type 1 'SecureStorage' block supplies the EnScrypt parameters to convert a user-supplied “local access passcode” into a 256-bit symmetric key,
+-- and also contains both the plaintext and encrypted data managed by that password.
 data SecureStorageBlock1
   = SecureStorageBlock1
-    { ss1CryptoIV     :: ByteString
-    , ss1ScryptSalt   :: ByteString
-    , ss1ScryptLogN   :: Word8
-    , ss1ScryptIter   :: Word32
-    , ss1Flags        :: ClientFlags
-    , ss1HintLen      :: Word8
-    , ss1HintIdle     :: Word16
-    , ss1PlainExtra   :: ByteString
-    , ss1MasterKey    :: MasterKey
-    , ss1LockKey      :: PrivateKey
-    , ss1UnlockKey    :: UnlockKey
-    , ss1EncryptExtra :: ByteString
-    , ss1VerifyTag    :: ByteString
+    { ss1CryptoIV     :: ByteString    -- ^ init vector for auth/encrypt
+    , ss1ScryptSalt   :: ByteString    -- ^ update for password change
+    , ss1ScryptLogN   :: Word8         -- ^ memory consumption factor
+    , ss1ScryptIter   :: Word32        -- ^ time consumption factor
+    , ss1Flags        :: ClientFlags   -- ^ 16 binary flags
+    , ss1HintLen      :: HintLength    -- ^ number of chars in hint
+    , ss1PwVerifySec  :: PWHashingTime -- ^ seconds to run PW EnScrypt
+    , ss1HintIdle     :: Word16        -- ^ idle minutes before wiping PW
+    , ss1PlainExtra   :: ByteString    -- ^ extended binary data not in spec as of yet
+    , ss1MasterKey    :: MasterKey     -- ^ encrypted identity master key
+    , ss1LockKey      :: PrivateKey    -- ^ encrypted identity lock key
+    , ss1UnlockKey    :: UnlockKey     -- ^ optional identity unlock key for previous identity (compare to 'emptyUnlockKey')
+    , ss1EncryptExtra :: ByteString    -- ^ extended encrypted data not in spec as of yet
+    , ss1VerifyTag    :: ByteString    -- ^ signature to validate no external changes has been made
     }
 
+-- | Type 2 - Rescue code encrypted data
+--
+-- The type 2 'SecureStorage' block supplies the EnScrypt parameters to convert a user-supplied “emergency rescue code” into a 256-bit symmetric key
+-- for use in decrypting the block's embedded encrypted emergency rescue code.
+data SecureStorageBlock2
+  = SecureStorageBlock2
+    { ss2ScryptSalt   :: ByteString      -- ^ update for password change
+    , ss2ScryptLogN   :: Word8           -- ^ memory consumption factor
+    , ss2ScryptIter   :: Word32          -- ^ time consumption factor
+    , ss2UnlockKey    :: IdentityUnlock  -- ^ emergency rescue code
+    , ss1EncryptExtra :: ByteString      -- ^ extended encrypted data not in spec as of yet
+    , ss2VerifyTag    :: ByteString      -- ^ signature to validate no external changes has been made
+    }
+
+-- | This two-byte value contains a set of individual single-bit flags corresponding to options offered by SQRL's user-interface.
 type ClientFlags = Word16
+
+-- | This one-byte value specifies the number of characters used in password hints. The default is 4 characters. A value of zero disables hinting and causes the SQRL client to prompt its user for their full access password whenever it's required.
+type HintLength = Word8
+
+-- | This one-byte value specifies the length of time SQRL's EnScrypt function will run in order to deeply hash the user's password to generate the Identity Master Key's (IMK) symmetric key. SQRL clients are suggested to default this value to five seconds with one second as a minimum. It should not be possible for the user to circumvent at least one second of iterative hashing on any platform.
+type PWHashingTime = Word8
 
 -- | This requests, and gives the SQRL client permission, to briefly check-in with its publisher to see whether any updates to this software have been made available.
 clientFlagAutoUpdate :: ClientFlags
 clientFlagAutoUpdate = 0x0001
 
--- | Where a SQRL client is loaded with multiple identities, this prevents the client from assuming any “current user” and causes it to prompt its operator for which identity should be used for every authentication. This can be useful when multiple users share a computer to keep any user from inadvertently attempting to use another user's identity.
+-- | Where a SQRL client is loaded with multiple identities, this prevents the client from assuming any “current user” and
+-- causes it to prompt its operator for which identity should be used for every authentication.
+-- This can be useful when multiple users share a computer to keep any user from inadvertently attempting to use another user's identity.
 clientFlagNoCurrentProfile :: ClientFlags
 clientFlagNoCurrentProfile = 0x0002
 
--- | This adds the @option=sqrlonly@ string to every client transaction. When this option string is present in any properly signed client transaction, this requests the server to set a flag in the user account that will cause the web server to subsequently disable all traditional non-SQRL account logon authentication such as username and password.
+-- | This adds the @option=sqrlonly@ string to every client transaction. When this option string is present in any properly signed client transaction,
+-- this requests the server to set a flag in the user account that will cause the web server to subsequently disable all traditional
+-- non-SQRL account logon authentication such as username and password.
 clientFlagSQRLOnly :: ClientFlags
 clientFlagSQRLOnly = 0x0004
 
--- | This adds the @option=hardlock@ string to every client transaction. When this option string is present in any properly signed client transaction, this requests the server to set a flag in the user account that will cause the web server to subsequently disable all “out of band” (non-SQRL) account identity recovery options such as “what was your favorite pet's name.”
+-- | This adds the @option=hardlock@ string to every client transaction. When this option string is present in any properly signed client transaction,
+-- this requests the server to set a flag in the user account that will cause the web server to subsequently disable all “out of band” (non-SQRL)
+-- account identity recovery options such as “what was your favorite pet's name.”
 clientFlagSQRLOnly :: ClientFlags
 clientFlagSQRLOnly = 0x0008
 
--- | When set, this bit instructs the SQRL client to notify its user when the web server indicates that an IP address mismatch exists between the entity that requested the initial logon web page containing the SQRL link URL (and probably encoded into the SQRL link URL's “nut”) and the IP address from which the SQRL client's query was received for this reply.
+-- | When set, this bit instructs the SQRL client to notify its user when the web server indicates that an IP address mismatch exists between the entity
+-- that requested the initial logon web page containing the SQRL link URL (and probably encoded into the SQRL link URL's “nut”) and the IP address
+-- from which the SQRL client's query was received for this reply.
 clientFlagWarnMITM :: ClientFlags
 clientFlagWarnMITM = 0x0010
 
--- | When set, this bit instructs the SQRL client to wash any existing local password and hint data from RAM upon notification that the system is going to sleep in any way such that it cannot be used. This would include sleeping, hibernating, screen blanking, etc.
+-- | When set, this bit instructs the SQRL client to wash any existing local password and hint data from RAM upon notification that the system
+-- is going to sleep in any way such that it cannot be used. This would include sleeping, hibernating, screen blanking, etc.
 clientFlagDiscardOnBlack :: ClientFlags
 clientFlagDiscardOnBlack = 0x0020
 
 -- | When set, this bit instructs the SQRL client to wash any existing local password and hint data from RAM upon notification that the current user is being switched.
 --
--- Notice: This could be interpreted as refering to the SQRL profile as in 'clientFlagNoCurrentProfile', but in actuality the "user" above is the user controlled by the OS. I could see it being used either way, though.
+-- Notice: This could be interpreted as refering to the SQRL profile as in 'clientFlagNoCurrentProfile', but in actuality the "user" above is the user controlled by the OS.
+-- I could see it being used either way, though.
 clientFlagDiscardOnUserSwitch :: ClientFlags
 clientFlagDiscardOnUserSwitch = 0x0040
 
--- | When set, this bit instructs the SQRL client to wash any existing local password and hint data from RAM when the system has been user-idle (no mouse or keyboard activity) for the number of minutes specified by the two-byte idle timeout.
+-- | When set, this bit instructs the SQRL client to wash any existing local password and hint data from RAM when the system has been user-idle (no mouse or keyboard activity)
+-- for the number of minutes specified by the two-byte idle timeout.
 --
--- Notice: The idle time in 'SecureStorageBlock1' is in minutes, when time=0 then no hint is allowed. It is quite clear that this is idle system-wide and not only in usage of SQRL. But since the idle time is allowed to be more than a month; a developer could see this as clearing the hint after being idle in the sense of no SQRL authentications for the specified amounts of minutes.
+-- Notice: The idle time in 'SecureStorageBlock1' is in minutes, when time=0 then no hint is allowed. It is quite clear that this is idle system-wide and not only in usage of SQRL.
+-- But since the idle time is allowed to be more than a month;
+-- a developer could see this as clearing the hint after being idle in the sense of no SQRL authentications for the specified amounts of minutes if there is no reliable way to detect other activity.
 clientFlagDiscardOnIdle :: ClientFlags
 clientFlagDiscardOnIdle = 0x0080
 
@@ -92,30 +131,99 @@ instance Binary SecureStorageBlock1 where
        <*> putByteString (ss1VerifyTag b)
 
 
+instance Binary SecureStorageBlock2 where
+  get = do blocklen <- getWord16
+           if blocklen < 73 then fail "Block too small"
+             else do blockt <- getWord16
+                     if blockt /= 2 then fail $ "Block type mismatch expected 2 got " ++ show blockt
+                       else SecureStorageBlock2
+                            <$> getByteString 16 <*> getWord8 <*> getWord32       -- scrypt params
+                            <*> getByteString 32                                  -- encrypted keys
+                            <*> getByteString (blocklen - 73)                     -- additional encrypted data
+                            <*> getByteString 16                                  -- auth tag
+  put b =  putWord16 (157 + BS.length (ss1PlainExtra b) + BS.length (ss1EncryptExtra b)) <*> putWord16 1 
+       <*> putWord16 (45 + BS.length (ss1PlainExtra b))
+       <*> putByteString (ss1CryptoIV b) <*> putByteString (ss1ScryptSalt b)
+       <*> putWord8 (ss1ScryptLogN b) <*> putWord32 (ss1ScryptIter b)
+       <*> putWord16 (ss1Flags b) <*> putWord8 (ss1HintLen b) <*> putWord16 (ss1HintIdle b)
+       <*> putByteString (ss1PlainExtra b)
+       <*> putByteString (ss1MasterKey b) <*> putByteString (ss1LockKey b)
+       <*> putByteString (ss1UnlockKey b) <*> putByteString (ss1EncryptExtra b)
+       <*> putByteString (ss1VerifyTag b)
+
+
 -- | A collection of related data connected to a specific SQRL profile.
 data SecureStorageBlock =
-  Block00001 SecureStorageBlock1 -- ^ The most basic of storage blocks. Contains information about master key and encryption settings.
+  Block00001 SecureStorageBlock1     -- ^ The most basic of storage blocks. Contains information about master key and encryption settings.
+  Block00002 SecureStorageBlock2     -- ^ Encrypted rescue code.
   BlockOther Int LBS.ByteString      -- ^ Any other block not supported by the specification at the time of writing, or chosen not to implement. Pull requests are welcome.
 
 -- | A secure storage for a SQRL profile. Contains encrypted keys and SQRL settings.
-data SecureStorage = SecureStorage String [SecureStorageBlock]
+data SecureStorage = SecureStorage Bool String [SecureStorageBlock]
 
 -- | Get the whole block as a lazy 'LBS.ByteString'.
 secureStorageData :: SecureStorageBlock -> LBS.ByteString
 secureStorageData (Block00001 b) = encode b
+secureStorageData (Block00002 b) = encode b
 secureStorageData (BlockOther _ bs) = bs
 
 -- | Get a structured version of the data contained by the block of type 1.
 secureStorageData1 :: SecureStorage -> Maybe SecureStorageBlock1
-secureStorageData1 (SecureStorage _ ss) = case find ((==) 1 . secureStorageType) ss of
+secureStorageData1 (SecureStorage _ _ ss) = case find ((==) 1 . secureStorageType) ss of
   Just (Block00001 b) -> Just b
+  _ -> Nothing
+
+-- | Get a structured version of the data contained by the block of type 2.
+secureStorageData2 :: SecureStorage -> Maybe SecureStorageBlock2
+secureStorageData2 (SecureStorage _ _ ss) = case find ((==) 2 . secureStorageType) ss of
+  Just (Block00002 b) -> Just b
   _ -> Nothing
 
 -- | Get something specific out of the 'SecureStorageBlock'. Currently only accepts first block of each type.
 secureStorageBlock :: Int -> SecureStorage -> Get a -> Maybe a
-secureStorageBlock bt (SecureStorage _ ss) f = case find ((==) bt . secureStorageType) ss of
+secureStorageBlock bt (SecureStorage _ _ ss) f = case find ((==) bt . secureStorageType) ss of
   Nothing -> Nothing
   Just sb -> case runGetOrFail f $ secureStorageData sb of
     Left _ -> Nothing
     Right (_, _, r) -> Just r
+
+-- | Open a 'SecureStorage' contained within a 'LBS.ByteString'.
+openSecureStorage' :: String -> LBS.ByteString -> Either String SecureStorage
+openSecureStorage' fn bs = case runGet (oss []) bs of
+  Left (_, pos, err) -> Left $ err ++ " (at position " ++ show pos ++ ")"
+  Right (_, _, rslt) -> SecureStorage False fn $ reverse rslt
+  where oss :: [SecureStorageBlock] -> Get [SecureStorageBlock]
+        oss p = isEmpty >>= \e -> if e then p else do
+          (l, t) <- (,) <$> getWord16 <*> getWord16
+          r <- case t of
+           1 -> Block00001 <$> get
+           2 -> Block00002 <$> get
+           _ -> BlockOther (fromIntegral t) <$> getLazyByteString $ fromIntegral (l - 32)
+          let r' = r : p in seq r' $ oss r'
+
+-- | Open a 'SecureStorage' contained within a file.
+openSecureStorage :: FilePath -> IO (Either String SecureStorage)
+openSecureStorage fp = withBinaryFile fp ReadMode (openSecureStorage' <$> LBS.hGetContent)
+
+-- | Turn a 'SecureStorage' into a lazy 'LBS.ByteString'.
+saveSecureStorage' :: SecureStorage -> LBS.ByteString
+saveSecureStorage' (SecureStorage _ _ ss) = runPut $ mapM_ sss ss
+  where sss :: SecureStorageBlock -> Put
+        sss x = case x of
+          Block00001 x'  -> put x'
+          Block00002 x'  -> put x'
+          BlockOther i b -> putWord16 (fromIntegral $ LBS.length b + 32) >> putWord16 (formIntegral i) >> putLazyByteString b
+
+-- | Saves any changes made to the SecureStorage.
+saveSecureStorage :: SecureStorage -> IO ()
+saveSecureStorage ss@(SecureStorage True fp _) = LBS.writeFile fp $ saveSecureStorage' ss
+saveSecureStorage _ = return ()
+
+-- | Creates an in memory copy of the 'SecureStorage'. This may then be changed and/or saved without affecting the previous storage.
+--
+-- > -- make a copy of the storage
+-- > openSecureStorage "original.ssss" >>= saveSecureStorage . copySecureStorage "copy.ssss" . either (\err -> error err) id
+copySecureStorage :: FilePath -> SecureStorage -> SecureStorage
+copySecureStorage fp (SecureStorage _ _ ss) = SecureStorage False fp ss
+
 
