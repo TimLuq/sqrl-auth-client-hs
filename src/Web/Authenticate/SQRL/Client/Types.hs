771fd99cbc01bd4e3eb32a8eded4052523e5e846
Tim Lundqvist <timluq@gmail.com>
2015-07-22 22:01:52 +0200

    Update Types.hs

--- src/Web/Authenticate/SQRL/Client/Types.hs
+++ src/Web/Authenticate/SQRL/Client/Types.hs
@@ -1,8 +1,14 @@
+{-# LANGUAGE MultiParamTypeClasses, FunctionalDependencies #-}
 module Web.Authenticate.SQRL.Client.Types where
 
+import Web.Authenticate.SQRL.Types
 
 import Data.ByteString (ByteString)
 import Data.Text (Text)
+import Control.Exception (SomeException)
+import Data.Maybe (fromMaybe)
+
+import qualified Crypto.Ed25519.Exceptions as ED25519
 
 type FullRealm = Text
 type Password = Text
@@ -11,9 +17,21 @@ newtype PublicLockKey = PublicLockKey { publicLockKey :: ByteString }
 
 newtype RescueCode         = RescueCode { rescueCode :: Text }
 --newtype PrivateIdentityKey = PrivateIdentityKey { privateIdentityKey :: ByteString }
-newtype PrivateMasterKey   = PrivateMasterKey   { privateMasterKey   :: ByteString }
-newtype PrivateUnlockKey   = PrivateUnlockKey   { privateUnlockKey   :: ByteString }
-newtype PrivateLockKey     = PrivateLockKey     { privateLockKey     :: ByteString }
+newtype PrivateMasterKey   = PrivateMasterKey   { privateMasterKey   :: ByteString } deriving (Show, Eq)
+newtype PrivateUnlockKey   = PrivateUnlockKey   { privateUnlockKey   :: ByteString } deriving (Show, Eq)
+newtype PrivateLockKey     = PrivateLockKey     { privateLockKey     :: ByteString } deriving (Show, Eq)
+
+class (PublicKey pub, DomainKey priv, Signature sig) => KeyPair priv pub sig | priv -> pub sig where
+  -- | Generates a public key from a 'DomainKey'.
+  generatePublic :: priv -> pub
+  generatePublic = mkPublicKey . ED25519.exportPublic . ED25519.generatePublic . fromMaybe (error "KeyPair.generatePublic: importPrivate returned Nothing.") . ED25519.importPrivate . domainKey
+  signData :: priv -> pub -> ByteString -> sig
+  signData priv' pub' bs =
+    let ED25519.Sig sig = ED25519.sign bs priv pub
+        priv = fromMaybe (error "KeyPair.signData: importPrivate returned Nothing.") $ ED25519.importPrivate $ domainKey priv'
+        pub  = fromMaybe (error "KeyPair.signData: importPublic returned Nothing.")  $ ED25519.importPublic  $ publicKey pub'
+    in mkSignature sig
+instance KeyPair DomainIdentityKey IdentityKey IdentitySignature
 
 class PrivateKey k where
   privateKey :: k -> ByteString
@@ -29,8 +47,8 @@ instance PrivateKey PrivateLockKey where
   privateKey = privateLockKey
   mkPrivateKey = PrivateLockKey
 
-newtype DomainLockKey      = DomainLockKey      { domainLockKey      :: ByteString }
-newtype DomainIdentityKey  = DomainIdentityKey  { domainIdentityKey  :: ByteString }
+newtype DomainLockKey      = DomainLockKey      { domainLockKey      :: ByteString } deriving (Show, Eq)
+newtype DomainIdentityKey  = DomainIdentityKey  { domainIdentityKey  :: ByteString } deriving (Show, Eq)
 
 
 class DomainKey k where
@@ -43,3 +61,17 @@ instance DomainKey DomainLockKey where
 instance DomainKey DomainIdentityKey where
   domainKey = domainIdentityKey
   mkDomainKey = DomainIdentityKey
+
+
+data ClientErr
+  = ClientErrNone
+  | ClientErrLoginAborted
+  | ClientErrAskAborted
+  | ClientErrAssocAborted
+  | ClientErrNoProfile
+  | ClientErrWrongPassword
+  | ClientErrSecureStorage String
+  | ClientErrDecryptionFailed String
+  | ClientErrThrown SomeException
+  | ClientErrOther String
+  deriving (Show)
